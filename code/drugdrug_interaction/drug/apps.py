37ce755649922dbc5f296d70d78d9248eb550422
王岚馨 <13450313+xiaolanxiner@user.noreply.giitee.com>
2023-10-31 23:18:04 +0800

    初步建model，详情类、相互作用类、介绍类

--- code/drugdrug_interaction/drug/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DrugConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'drug'
--- code/drugdrug_interaction/drugdrug_interaction/settings.py
+++ code/drugdrug_interaction/drugdrug_interaction/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drug',  # 注册drug应用
 ]
 
 MIDDLEWARE = [
