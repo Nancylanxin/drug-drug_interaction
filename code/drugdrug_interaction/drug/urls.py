3bb7ea7727a50e03dab26a713bf67be0b1442418
王岚馨 <13450313+xiaolanxiner@user.noreply.giitee.com>
2023-11-01 15:14:23 +0800

    修改了整个后端

--- code/drugdrug_interaction/drug/urls.py
+++ code/drugdrug_interaction/drug/urls.py
@@ -5,6 +5,4 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('drugDetail', views.detail, name='detail'),
-    path('interaction', views.interaction, name='interaction'),
 ]
