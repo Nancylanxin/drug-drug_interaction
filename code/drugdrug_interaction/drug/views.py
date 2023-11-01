3ce91024cfae2d00bbd1528405ad238ffd7aa91a
王岚馨 <13450313+xiaolanxiner@user.noreply.giitee.com>
2023-11-01 09:52:34 +0800

    更改视图函数，尝试连到数据库

--- code/drugdrug_interaction/drug/views.py
+++ code/drugdrug_interaction/drug/views.py
@@ -1,17 +1,36 @@
-from django.shortcuts import render
+from filecmp import cmp
 
 # Create your views here.
 from django.shortcuts import render
+import sqlite3
+from pypinyin import pinyin, Style
 
-from .models import Introduction
+
+# 自定义排序规则函数
+def chinese_pinyin_collation(s1, s2):
+    pinyin1 = pinyin(s1, style=Style.NORMAL)
+    pinyin2 = pinyin(s2, style=Style.NORMAL)
+    return cmp(pinyin1, pinyin2)  # 比较拼音
+
+
+# 连接到 SQLite 数据库
+conn = sqlite3.connect('E:\pycharm_project\final_drug\code\drugdrug_interaction\db.sqlite')
+
+# 注册自定义排序规则
+conn.create_collation('chinese_pinyin', chinese_pinyin_collation)
+
+# 执行查询并使用自定义排序规则
+cursor = conn.cursor()
+cursor.execute('SELECT * FROM detail ORDER BY name COLLATE chinese_pinyin')
+result = cursor.fetchall()
 
 
 def index(request):
-    drug_list = Introduction.objects.all()
+    # drug_list = Introduction.objects.all()
     return render(request, 'drug/index.html', context={
         'title': '药药相互',
         'welcome': '欢迎访问我的drug首页',
-        'drug_list': drug_list
+        # 'drug_list': drug_list
     })
 
 
@@ -28,4 +47,3 @@ def interaction(request):
         'title': '药药相互',
         'welcome': '欢迎访问我的drug首页'
     })
-
