51c8bfc0fd27564a6550a86c070187632f46b966
王岚馨 <13450313+xiaolanxiner@user.noreply.giitee.com>
2023-11-01 18:09:22 +0800

    前后端框架搭好，大体流程连通

--- code/drugdrug_interaction/drug/urls.py
+++ code/drugdrug_interaction/drug/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('', views.interaction, name='interaction'),
+    path('drug.html', views.detail, name='drug')
 ]
--- code/drugdrug_interaction/drug/views.py
+++ code/drugdrug_interaction/drug/views.py
@@ -7,3 +7,16 @@ def index(request):
         'welcome': '欢迎访问我的博客首页'
     })
 
+
+def interaction(request):
+    from code.drugdrug_interaction.drug.models import Interaction
+    drug_list = Interaction.objects.all()
+
+    return render(request, 'drug/index.html', context={
+        'drug_list': drug_list,
+    })
+
+
+def detail(request):
+    return render(request, 'drug/drug.html', context={
+    })
