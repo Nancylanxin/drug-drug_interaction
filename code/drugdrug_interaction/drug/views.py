e20f1e46dd16b3c68dcd238d5433e36c4f16b10a
王岚馨 <13450313+xiaolanxiner@user.noreply.giitee.com>
2023-11-01 10:04:10 +0800

    修改views.py，已连接到数据库，可按中文拼音首字母排序展示数据库记录

--- code/drugdrug_interaction/drug/views.py
+++ code/drugdrug_interaction/drug/views.py
@@ -2,27 +2,40 @@ from filecmp import cmp
 
 # Create your views here.
 from django.shortcuts import render
+import os
 import sqlite3
 from pypinyin import pinyin, Style
 
 
 # 自定义排序规则函数
 def chinese_pinyin_collation(s1, s2):
-    pinyin1 = pinyin(s1, style=Style.NORMAL)
-    pinyin2 = pinyin(s2, style=Style.NORMAL)
-    return cmp(pinyin1, pinyin2)  # 比较拼音
+    pinyin1 = ''.join([p[0] for p in pinyin(s1, style=Style.NORMAL)])
+    pinyin2 = ''.join([p[0] for p in pinyin(s2, style=Style.NORMAL)])
+    if pinyin1 < pinyin2:
+        return -1
+    elif pinyin1 > pinyin2:
+        return 1
+    else:
+        return 0
 
 
 # 连接到 SQLite 数据库
-conn = sqlite3.connect('E:\pycharm_project\final_drug\code\drugdrug_interaction\db.sqlite')
+conn = sqlite3.connect('E:\\pycharm_project\\final_drug\\code\\drugdrug_interaction\\db.sqlite')
 
 # 注册自定义排序规则
 conn.create_collation('chinese_pinyin', chinese_pinyin_collation)
 
 # 执行查询并使用自定义排序规则
 cursor = conn.cursor()
-cursor.execute('SELECT * FROM detail ORDER BY name COLLATE chinese_pinyin')
+cursor.execute('SELECT * FROM drug_detail ORDER BY name COLLATE chinese_pinyin')
 result = cursor.fetchall()
+
+# 打印排序结果
+for row in result:
+    print(row)
+
+# 关闭数据库连接
+conn.close()
 
 
 def index(request):
